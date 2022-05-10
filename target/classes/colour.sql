5a3ea7253aa83d63aca3ab81fbf360b1161e7a0f
tothmelinda <tothmelinda75@yahoo.com>
2022-05-10 22:45:37 +0300

    first commit

---
+++ target/classes/colour.sql
@@ -0,0 +1,25 @@
+INSERT INTO CAR_TYPE (colour) VALUES('blue');
+INSERT INTO CAR_TYPE (colour) VALUES('grey');
+INSERT INTO CAR_TYPE (colour) VALUES('red');
+INSERT INTO CAR_TYPE (colour) VALUES('black');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+INSERT INTO CAR_TYPE (colour) VALUES('grey');
+INSERT INTO CAR_TYPE (colour) VALUES('blue');
+INSERT INTO CAR_TYPE (colour) VALUES('black');
+INSERT INTO CAR_TYPE (colour) VALUES('red');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+INSERT INTO CAR_TYPE (colour) VALUES('silver');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+INSERT INTO CAR_TYPE (colour) VALUES('blue');
+INSERT INTO CAR_TYPE (colour) VALUES('grey');
+INSERT INTO CAR_TYPE (colour) VALUES('red');
+INSERT INTO CAR_TYPE (colour) VALUES('black');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+INSERT INTO CAR_TYPE (colour) VALUES('grey');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+INSERT INTO CAR_TYPE (colour) VALUES('black');
+INSERT INTO CAR_TYPE (colour) VALUES('silver');
+INSERT INTO CAR_TYPE (colour) VALUES('black');
+INSERT INTO CAR_TYPE (colour) VALUES('white');
+
+
